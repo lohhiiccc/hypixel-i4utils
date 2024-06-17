efb55ec66210e1cc6355c6087cbd892d8cf57635
lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>
2024-06-18 00:19:45 +0200

    update hitbox ashfang config

--- features/ashfang/ashfangclass.js
+++ features/ashfang/ashfangclass.js
@@ -10,8 +10,11 @@ class Ashfang {
         this.black = [];
         this.bullet = [];
     }
-    drawBox(x, y, z, r, g, b , a, h){
+    drawbeam(x, y, z, r, g ,b) {
         renderBeaconBeam(x - 0.5, y - 2 , z - 0.5, r, g , b, true);
+    }
+    drawBox(x, y, z, r, g, b , a, h){
+        if (!Settings.AshfangHitboxToogle) return;
         RenderLib.drawInnerEspBox(x, y - h, z , 1, h, r, g, b, a, false);
         RenderLib.drawEspBox(x - 0.1, y - h - 0.05, z , 1.1, h + 0.1, 1, 1, 1, 1, false);
     }
@@ -25,27 +28,30 @@ class Ashfang {
     render() {
         if (Settings.AshfangHitboxToogle)
             RenderLib.drawEspBox(-484 -0.5, 135 , -1016 +0.5, 2, 3, 1, 1, 1, 1, true);
-        if (Settings.AshfangBlueToogle) {
-            this.blue.forEach((entity) => {
-                this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 1, 0.5, 2);
-            })
-        }
-        if (Settings.AshfangRedToogle) {
-            this.red.forEach((entity) => {
-                this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 1, 0, 0, 0.5, 2);
-            })
-        }
-        if (Settings.AshfangBlackToogle) {
-            this.black.forEach((entity) => {
-                this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 0, 0.5, 2);
-            })
-        }
-        if (Settings.AshfangBulletToogle) {
-            this.bullet.forEach((entity) => {
-                this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 1, 1, 1, 0.5, 1);
-            })
-        }
-
+       this.blue.forEach((entity) => {
+           if (Settings.AshfangBlueToogle)
+               this.drawbeam(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 1, 0.5);
+           if (Settings.AshfangHitboxToogle)
+               this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 1, 0.5, 2);
+       })
+       this.red.forEach((entity) => {
+           if (Settings.AshfangRedToogle)
+               this.drawbeam(entity.getX(), entity.getY(), entity.getZ(), 1, 0, 0, 0.5);
+           if (Settings.AshfangHitboxToogle)
+               this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 1, 0, 0, 0.5, 2);
+       })
+       this.black.forEach((entity) => {
+           if (Settings.AshfangBlackToogle)
+               this.drawbeam(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 0, 0.5);
+           if (Settings.AshfangHitboxToogle)
+               this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 0, 0, 0, 0.5, 2);
+       })
+       this.bullet.forEach((entity) => {
+           if (Settings.AshfangBulletToogle)
+               this.drawbeam(entity.getX(), entity.getY(), entity.getZ(), 1, 1, 1, 0.5);
+           if (Settings.AshfangHitboxToogle)
+               this.drawBox(entity.getX(), entity.getY(), entity.getZ(), 1, 1, 1, 0.5, 2);
+       })
     }
 }
 
