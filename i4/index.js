bb0cf86dbf245ddcbe84b5bfd2c44601916d21bc
loic <lrio@student.42lyon.fr>
2024-06-18 00:27:31 +0200

    remove useless files

--- i4/index.js
+++
@@ -1,9 +0,0 @@
-import "./features/gyro"
-import "./features/i4/i4";
-
-
-
-import Settings from "./config";
-import { Color } from "Vigilance";
-
-register("command", () => Settings.openGUI()).setName("i4");
